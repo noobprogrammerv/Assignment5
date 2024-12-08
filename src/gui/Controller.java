75efdf7f1d6e512b451acfa3cc1bb105103c3b0b
alexgit <resiga.alex@yahoo.com>
2024-12-08 15:19:12 +0200

    4 buttons ready

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -1,6 +1,9 @@
 package gui;
 
 import domain.Car;
+import domain.Reservation;
+import domain.Identifiable;
+import javafx.beans.value.ObservableObjectValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -15,31 +18,33 @@ import java.util.ArrayList;
 public class Controller {
     CarsService serv;
     ObservableList<Car> carsList;
+    ObservableList<Car> resultsList;
 
     @FXML
     private ListView<Car> carsListView;
-
+    @FXML
+    private ListView<Car> resultsListView;
     @FXML
     private TextField idTextField;
-
     @FXML
     private TextField makeTextField;
-
     @FXML
     private TextField modelTextField;
-
     @FXML
     private TextField yearTextField;
 
     @FXML
-    private Button removeButton;
-
-
+    private Button buttonRemoveCar;
     @FXML
-    private Button buttonAdd;
-
+    private Button buttonAddCar;
     @FXML
-    void buttonAddHandler(ActionEvent event){
+    private Button buttonUpdateCar;
+    @FXML
+    private Button buttonShowCarById;
+
+    // 1) Add car
+    @FXML
+    void buttonAddCarHandler(ActionEvent event){
         String id = idTextField.getText();
         String make = makeTextField.getText();
         String model = modelTextField.getText();
@@ -51,16 +56,16 @@ public class Controller {
         modelTextField.clear();
         yearTextField.clear();
     }
-
+    // 2) remove car
     @FXML
-    void buttonRemoveHandler(ActionEvent event){
+    void buttonRemoveCarHandler(ActionEvent event){
         Car car = carsListView.getSelectionModel().getSelectedItem();
         serv.deleteCar(car.getId());
         resetObservableList();
     }
-
+    // 3) update car
     @FXML
-    void buttonUpdateHandler(ActionEvent event){
+    void buttonUpdateCarHandler(ActionEvent event){
         String id = idTextField.getText();
         String make = makeTextField.getText();
         String model = modelTextField.getText();
@@ -72,17 +77,36 @@ public class Controller {
         modelTextField.clear();
         yearTextField.clear();
     }
-        /// show car by id
-      // show car by id
-    /*void buttonShowCarByIdHandler(ActionEvent event){
+    // 4) show car by id
+    @FXML
+    void buttonShowCarByIdHandler(ActionEvent event){
+        String id = idTextField.getText();
+        showCarInList(id);
 
-    }*/
+    }
+    void showCarInList(String id) {
+        Car car = serv.findCarById(id);
+        if (car != null) {
+            ObservableList<Car> carList = FXCollections.observableArrayList(car);
+            resultsListView.setItems(carList);
+        } else {
+            // car not found, we clear the list
+            resultsListView.setItems(FXCollections.observableArrayList());
+        }
+    }
+    // 5) show all cars
+    void showAllCarsInList(){
+        ArrayList<Car> cars = this.serv.getAll();
+        carsList = FXCollections.observableArrayList(cars);
+        resultsListView.setItems(carsList);
+    }
 
     void resetObservableList(){
         ArrayList<Car> cars = this.serv.getAll();
         carsList = FXCollections.observableArrayList(cars);
         carsListView.setItems(carsList);
     }
+
 
     public Controller(CarsService serv) {
         this.serv = serv;
@@ -94,3 +118,22 @@ public class Controller {
         carsListView.setItems(carsList);
     }
 }
+
+/*
+ADDCAR                              1 - done
+REMOVECAR                           2 - done
+UPDATECAR                           3 - done
+SHOWCARBYID                         4 - done
+SHOWALLCARS                         5
+FILTERCARSBYYEAR                    6
+ADDRESERVATION                      7
+REMOVERESERVATION                   8
+UPDATERESERVATION                   9
+SHOWRESERVATIONBYID                 10
+SHOWALLRESERVATIONS                 11
+GETALLCARSWITHSAMEMAKE              12
+GETALLCARSWITHSAMEYEAR              13
+GETALLCARSRESERVEDAT                14
+GETALLCARSRESERVEDBEFORE            15
+GETALLCARSRESERVEDAFTER             16
+*/
